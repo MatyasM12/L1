82cbbfcb2c78e4e5a36611faf551c3cb3f956202
MatyasM12 <mihai_matyas@yahoo.com>
2017-10-22 19:24:33 +0300

    first
    first

---
+++ app/src/main/java/com/example/mikha/l1/MainAct.java
@@ -0,0 +1,179 @@
+package com.example.mikha.l1;
+
+import android.app.ProgressDialog;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainAct extends YouTubeBaseActivity {
+
+    private Button btn;
+    private Button videoBtn;
+    private boolean playPause;
+    private MediaPlayer mediaPlayer;
+    private ProgressDialog progressDialog;
+    private boolean initialStage = true;
+    private YouTubePlayerView youTubePlayerView;
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
+    //private YoutubePlayer.OnIn
+    public static final String YOUTUBE_API_KEY = "AIzaSyC9iF96dl6oB7F3KsMOBSjSu9N7XZVBhBA\t";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        //youtube stuff
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtubePlayer);
+        onInitializedListener = new OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("vW1hv37imjw");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        //youtube start video button
+        videoBtn = (Button) findViewById(R.id.vidBtn);
+        videoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize("AIzaSyC9iF96dl6oB7F3KsMOBSjSu9N7XZVBhBA", onInitializedListener);
+            }
+        });
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
+
+        btn = (Button) findViewById(R.id.audioStartBtn);
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        progressDialog = new ProgressDialog(this);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (!playPause) {
+                    btn.setText("Pause");
+                    if (initialStage) {
+                        new Player().execute("http://www.ssaurel.com/tmp/mymusic.mp3");
+                    } else {
+                        if (!mediaPlayer.isPlaying())
+                            mediaPlayer.start();
+                    }
+                    playPause = true;
+                }
+                else{
+                    btn.setText("Start!");
+                    if(mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                    }
+                    playPause = false;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(mediaPlayer != null){
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+
+    class Player extends AsyncTask<String, Void, Boolean>{
+        @Override
+        protected Boolean doInBackground(String... strings){
+            Boolean prepared = false;
+            try{
+                mediaPlayer.setDataSource(strings[0]);
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer){
+                        initialStage = false;
+                        btn.setText("Launch streaming");
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                    }
+                });
+                mediaPlayer.prepare();
+                prepared = true;
+            }catch(Exception ex){
+                Log.e("Myaudiostreamingapp", ex.getMessage());
+                prepared = false;
+            }
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            if(progressDialog.isShowing()){
+                progressDialog.cancel();
+            }
+            mediaPlayer.start();
+            initialStage = false;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setMessage("Buffering...");
+            progressDialog.show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
